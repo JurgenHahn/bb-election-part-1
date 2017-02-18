1bc242a9aa1d3e46c70fb1d9dd80b8146c9efa26
JurgenHahn <jurgenehahn@gmail.com>
2017-02-17 21:42:03 -0500

    assignment done

--- election.js
+++ election.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done(function(data){
 
+    var i = 0;
+
+    $.each(data.candidates, function(){
+      i++;
+      $('<li>').html(this.name, this.votes).appendTo('.list');
+      $('<li>').html(this.votes).appendTo('.list');
+    });
+  });
 });
